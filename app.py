270144ce488689cc51488ab6dff52a35ed0b4837
Feranmi Akinwale <feranmiphileo@gmail.com>
2025-05-24 21:10:29 +0200

    more robust

--- app.py
+++ app.py
@@ -2,19 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL to scrape
-url = "http://quotes.toscrape.com/"
+url = "https://quotes.toscrape.com/page/"
 
-# Send HTTP GET request
-response = requests.get(url)
+page_still_valid = True
+authors = set()
+page = 1
 
-# Parse the HTML content
-soup = BeautifulSoup(response.text, "html.parser")
+while page_still_valid:
 
-# Find all quote containers
-quotes = soup.find_all("div", class_="quote")
+    page_url = url+str(page)
+    # Send HTTP GET request
+    response = requests.get(page_url)
 
-# Loop through each quote block and extract quote text and author
-for quote in quotes:
-    text = quote.find("span", class_="text").text
-    author = quote.find("small", class_="author").text
-    print(f"{text} — {author}")
+    # Check to see if we're on the last page
+    if "No quotes found!" in response.text:
+        break
+
+    # Parse the HTML content
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Find all quote containers
+    quotes = soup.find_all("div", class_="quote")
+
+    # Loop through each quote block and extract quote text and author
+    for quote in quotes:
+        text = quote.find("span", class_="text").text
+        author = quote.find("small", class_="author").text
+        print(f"{text} — {author}")
+
+    page += 1
